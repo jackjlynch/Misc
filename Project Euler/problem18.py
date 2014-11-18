7af0fed41414f323fbce9905ac96d580d8cd0724
Jack Lynch <jacklynch@wharnal.com>
2014-11-18 12:59:36 -0500

    started euler 18

---
+++ Project Euler/problem18.py
@@ -0,0 +1,19 @@
+class TreeNode():
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+
+    def addChild(self, child)
+        self.children.add(child)
+
+    def getChildren(self):
+        return self.children
+
+
+def readFile(inputFile):
+    line = inputFile.readline()
+
+    while line is not None:
+        for value in line.split(" "):
+
+
